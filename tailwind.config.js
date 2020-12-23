9d0a9ed05ab2780f939b70ecf36f60298d46b59b
noltedennis <dennis_nolte@gmx.de>
2020-12-23 23:44:26 +0100

    First design iterations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
+  important: true,
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
+    extend: {
+      colors: {
+        // Configure your color palette here
+        denolte: '#ff6300',
+      }
+    },
   },
   variants: {
     extend: {},
