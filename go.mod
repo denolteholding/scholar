3b06e438aa5de11f87b213a1ad160ebc931ae354
noltedennis <dennis_nolte@gmx.de>
2020-12-26 13:50:22 +0100

    Some work on about page, major refactoring of Tailwind CSS integration

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module github.com/denolteholding/scholar
 
 go 1.15
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,47 @@
+const typography = require('@tailwindcss/typography');
+
+//const colorBrand = 'var(--color-pretty)';
+
+// Utils
+const round = (num) => num.toFixed(7).replace(/(\.[0-9]+?)0+$/, '$1').replace(/\.0$/, '');
+const rem = (px) => `${round(px / 16)}rem`;
+const em = (px, base) => `${round(px / base)}em`;
+const px = (px) => `${px}px`;
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  important: true,
-  theme: {
-    container: {
-      center: true,
-      padding: "1.5rem",
-    },
-    extend: {
-      colors: {
-        // Configure your color palette here
-        denolte: '#ff6300',
-      }
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	important: true, // See https://tailwindcss.com/docs/configuration#important
+	darkMode: false,
+	experimental: {
+		// See https://github.com/tailwindlabs/tailwindcss/pull/2159
+		applyComplexClasses: true
+	},
+	purge: {
+		enabled: process.env.HUGO_ENVIRONMENT === 'production',
+		content: [ './hugo_stats.json' ],
+		mode: 'all',
+		options: {
+			//whitelist: [ 'pl-1', 'pl-3' ],
+			defaultExtractor: (content) => {
+				let els = JSON.parse(content).htmlElements;
+				els = els.tags.concat(els.classes, els.ids);
+				return els;
+			}
+		}
+	},
+	theme: {
+		container: {
+		  center: true,
+		  padding: "1.5rem",
+		},
+		extend: {
+		  colors: {
+			// Configure your color palette here
+			denolte: '#ff6300',
+		  }
+		},
+	  },
+	  variants: {
+		extend: {},
+	  },
+	plugins: [ typography ],
+};
